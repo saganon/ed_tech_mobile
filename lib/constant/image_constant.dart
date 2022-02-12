14d45846ef83c2846b2edc055277ba517531ec39
shanon3 <shanonathooya@gmail.com>
2022-02-13 01:55:29 +0700

    Add splash and onboarding screen

---
+++ lib/constant/image_constant.dart
@@ -0,0 +1,10 @@
+class ImageConstant {
+  static const splashscreen =
+      'assets/image/on_boarding_assets/splash_screen.png';
+  static const illustration1 =
+      'assets/image/on_boarding_assets/illustration-1.png';
+  static const illustration2 =
+      'assets/image/on_boarding_assets/illustration-2.png';
+  static const illustration3 =
+      'assets/image/on_boarding_assets/illustration-3.png';
+}
